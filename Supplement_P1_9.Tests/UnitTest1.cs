147662ac22359b11968fc59c9f7d2ffbdd892a16
Mihiretu Jackson <mihiretujackson@gmail.com>
2025-02-27 16:37:10 -0500

    tests: added unit tests for RandomFloatingNumberGenerator

--- Supplement_P1_9.Tests/UnitTest1.cs
+++ Supplement_P1_9.Tests/UnitTest1.cs
@@ -3,8 +3,29 @@
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Exception_WithMessage()
     {
+         var exception = new InvalidSequenceException();
+        Assert.Equal("Invalid sequence detected: Three consecutive values ≤ 0.5", exception.Message);
 
+    }
+    [Fact]
+    public void ShouldGenerateNumbersBetween0And1()
+    {
+        var generator = new RandomFloatingNumberGenerator();
+        var numbers = generator.Take(15).ToList();
+
+        Assert.All(numbers, number => Assert.InRange(number, 0.0f, 1.0f)); // Expecting float values
+    }
+    [Fact]
+    public void ShouldThrowExceptionOnThreeConsecutiveLowNumbers()
+    {
+        var generator = new RandomFloatingNumberGenerator();
+        var enumerator = generator.GetEnumerator();
+
+        Assert.Throws<InvalidSequenceException>(() =>
+        {
+            while (enumerator.MoveNext()) { }
+        });
     }
 }
