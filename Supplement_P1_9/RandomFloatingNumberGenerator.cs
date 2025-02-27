4b675180d81ebe2fd671c28183f024c091f692be
Mihiretu Jackson <mihiretujackson@gmail.com>
2025-02-27 16:48:55 -0500

    doc:added documenation for RandomFloatingNumberGenerator class and methods in it

--- Supplement_P1_9/RandomFloatingNumberGenerator.cs
+++ Supplement_P1_9/RandomFloatingNumberGenerator.cs
@@ -6,6 +6,12 @@ public class RandomFloatingNumberGenerator: IEnumerable<float>
 {
     private Random _random = new Random();
 
+    /// <summary>
+    /// Generates an infinite sequence of random floating-point numbers between 0 and 1.
+    /// If three consecutive numbers are <= 0.5, an InvalidSequenceException is thrown.
+    /// </summary>
+    /// <returns>An enumerator that iterates over random float values.</returns>
+    /// <exception cref="InvalidSequenceException">Thrown when three consecutive numbers are <= 0.5.</exception>
     public IEnumerator<float> GetEnumerator()
     {
          int lowCount = 0;
@@ -25,6 +31,10 @@ public class RandomFloatingNumberGenerator: IEnumerable<float>
         }
     }
 
+    /// <summary>
+    /// Returns an enumerator that iterates through the collection.
+    /// </summary>
+    /// <returns>An IEnumerator instance.</returns>
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
 }
