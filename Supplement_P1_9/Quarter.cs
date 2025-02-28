fc0d4b2f42d57a5459ea3b6b744b8c5955d834ab
Mihiretu Jackson <mihiretujackson@gmail.com>
2025-02-27 21:42:14 -0500

    implentation: implemented menthods in Quarter class

--- Supplement_P1_9/Quarter.cs
+++ Supplement_P1_9/Quarter.cs
@@ -14,33 +14,40 @@ public class Quarter
         Value = value;
     }
 
-    public static bool operator ==(Quarter q1, Quarter q2)
+    private int GetQuarter()
     {
-        throw new NotImplementedException();
+        if (Value < 0.25) return 1;
+        if (Value < 0.5) return 2;
+        if (Value < 0.75) return 3;
+        return 4;
     }
 
-    public static bool operator !=(Quarter q1, Quarter q2)
+    public static bool operator ==(Quarter? q1, Quarter? q2)
     {
-        throw new NotImplementedException();
+        if (ReferenceEquals(q1, q2)) return true;
+        if (q1 is null || q2 is null) return false;
+        return q1.GetQuarter() == q2.GetQuarter();
     }
 
-    public static bool operator >(Quarter q1, Quarter q2)
-    {
-        throw new NotImplementedException();
-    }
+    public static bool operator !=(Quarter? q1, Quarter? q2) => !(q1 == q2);
+   
 
-    public static bool operator <(Quarter q1, Quarter q2)
-    {
-        throw new NotImplementedException();
-    }
+    public static bool operator >(Quarter q1, Quarter q2) => q1.Value > q2.Value;
 
-    public static bool operator >=(Quarter q1, Quarter q2)
-    {
-        throw new NotImplementedException();
-    }
+    public static bool operator <(Quarter q1, Quarter q2) => q1.Value < q2.Value;
 
-    public static bool operator <=(Quarter q1, Quarter q2)
-    {
-        throw new NotImplementedException();
-    }
+    public static bool operator >=(Quarter q1, Quarter q2) => q1.Value >= q2.Value;
+
+    public static bool operator <=(Quarter q1, Quarter q2) => q1.Value <= q2.Value;
+
+    public override bool Equals(object? obj)
+        {
+            return obj is Quarter q && this == q;
+        }
+
+        public override int GetHashCode()
+        {
+            return GetQuarter().GetHashCode();
+        }
 }
+
